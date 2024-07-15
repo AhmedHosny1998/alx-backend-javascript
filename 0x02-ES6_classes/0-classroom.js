c7ff201c7300f5f88df148cdc8d4c53ca5c40713
Ahmed Hosny <hosny4620@gmail.com>
2024-07-15 03:07:41 +0300

    class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
-class ClassRoom{
-    constructor(maxStudentsSize ){
-        this._maxStudentsSize = maxStudentsSize;
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
     }
-}
+  }
--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,3 +1,4 @@
-import Class from "./0-classroom.js";
-const r = new ClassRoom(10);
-console.log(r);
+import ClassRoom from "./0-classroom.js";
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize);
