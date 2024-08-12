41401e3f4af6a7b43668b26cf5a29a0b9e587ec8
AhmedHosny1998 <hosny4620@gmail.com>
2024-08-11 17:20:03 -0700

    data

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(students) {
-  if (students === Array) {
+  if (students instanceof Array) {
     return students.map((student) => student.id);
   }
   return [];
